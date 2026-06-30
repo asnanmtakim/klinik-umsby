095bdffa0775b46f6b0db65425abccda99655749
M. Asnan Mustakim <asnanmustakim126@gmail.com>
2026-06-30 10:17:01 +0700

    fix: disable menu item in SiteMenusSeeder

--- app/Database/Seeds/SiteMenusSeeder.php
+++ app/Database/Seeds/SiteMenusSeeder.php
@@ -132,7 +132,7 @@ class SiteMenusSeeder extends Seeder
                 'filter' => 'session',
                 'filter_value' => null,
                 'order' => 7,
-                'active' => 1,
+                'active' => 0,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s'),
             ],
